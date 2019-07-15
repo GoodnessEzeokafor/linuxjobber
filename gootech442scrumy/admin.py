e234a97a5101dccac5616ad93bda11c4b47516fd
GoodnessEzeokafor <gootech442@gmail.com>
2019-07-15 17:19:26 +0100

    Completed Lab 10

--- gootech442scrumy/admin.py
+++ gootech442scrumy/admin.py
@@ -3,6 +3,11 @@ from .models import ScrumyGoals, ScrumyHistory, GoalStatus
 # Register your models here.
 
 
-admin.site.register(ScrumyGoals)
+
+class ScrumyGoalsAdmin(admin.ModelAdmin):
+    list_display = ('goal_id','goal_name','created_by', 'moved_by', 'owner', 'goal_status', 'user')
+
+
+admin.site.register(ScrumyGoals,ScrumyGoalsAdmin)
 admin.site.register(ScrumyHistory)
 admin.site.register(GoalStatus)
