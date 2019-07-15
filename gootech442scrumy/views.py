776f49a6ce3c31f327b47ede8fd03747d48b8162
GoodnessEzeokafor <gootech442@gmail.com>
2019-07-15 17:31:24 +0100

    Completed Lab 11

--- gootech442scrumy/views.py
+++ gootech442scrumy/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from .models import (
+    GoalStatus,
+    ScrumyGoals,
+    ScrumyHistory
+)
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World")
+    goal_filter = ScrumyGoals.objects.filter(goal_name ="Learn Django")
+    return HttpResponse(goal_filter)
+    
